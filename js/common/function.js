10f0082036d3d4dd50ca42d930fa5efa0819bd26
floraCat <1781721016@qq.com>
2018-05-30 16:22:26 +0800

    com-5-30

--- js/common/function.js
+++ js/common/function.js
@@ -1,8 +1,24 @@
 
 
-/*获取Url参数值*/
-function getQueryString(key){
-	var reg = new RegExp("(^|&)"+key+"=([^&]*)(&|$)");
-	var result = window.location.search.substr(1).match(reg);
-	return result?decodeURIComponent(result[2]):null;
-}
+/*倒计时 - 分秒*/
+// 参数overTime未要倒数的时间戳
+countTime(overTime, callback) {
+	let self = this
+	if(overTime <= 0) { // 倒计完
+		clearTimeout(setTimeObj)
+		if (callback && 'function' === typeof callback) {
+			callback(false)
+		}
+		return
+	} else {
+		let data;
+		let _mTime = Math.floor(overTime / 1000 / 60 % 60);
+		let _sTime = Math.floor(overTime / 1000 % 60);
+		data.mTime = _mTime >= 10 ? _mTime + "分" : '0' + _mTime + "分";
+		data.sTime = _sTime >= 10 ? _sTime + "秒" : '0' + _sTime + "秒";
+		if (callback && 'function' === typeof callback) {
+			callback(data)
+		}
+	}
+	var setTimeObj = setTimeout(self.countTime, 1000);
+},
--- js/common/pattern.js
+++ js/common/pattern.js
@@ -1,7 +1,32 @@
 
 /*
 *
-* 格式处理
+* 格式化处理
 *
 */
+
+
+/*价格保留两位数*/
+function toPrice (val) {
+	return parseInt(val).toFixed(2);
+}
+
+
+/*n秒后的时间 - 时分秒*/
+function toTime (second) {
+	var d = new Date(new Date().getTime() + second * 60 * 1000);
+	var result = d.getHours() + ":"
+	    	  + ( d.getMinutes() >= 10 ? d.getMinutes() : ("0" + d.getMinutes()) ) + ":" 
+	    	  + ( d.getSeconds() >= 10 ? d.getSeconds() : ("0" + d.getSeconds()) )
+	return result;
+}
+
+/*n天后的日期 - 年月日*/
+function toDay (day) {
+	var d = new Date();
+    var today = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
+    var d2 = new Date(d);
+    d2.setDate(d.getDate() + day);
+	return d2.getFullYear() + "-" + (d2.getMonth()+1) + "-" + d2.getDate();
+}
 
